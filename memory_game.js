b935d518b39e997d5e3105fda5ca44510257a4ab
pvarsha22 <varshapatil498@gmail.com>
2021-01-11 12:17:46 +0530

    add scripting

---
+++ memory_game.js
@@ -0,0 +1,200 @@
+var library = {
+    pokemon: [
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980129/memory/Pokemon/Mewtwo.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980142/memory/Pokemon/Mew.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980154/memory/Pokemon/Articuno.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980164/memory/Pokemon/Zapdos.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980175/memory/Pokemon/Moltres.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980186/memory/Pokemon/Eevee.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980025/memory/Pokemon/Bulbasaur.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980083/memory/Pokemon/Charmander.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980101/memory/Pokemon/Squirtle.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980116/memory/Pokemon/Pikachu.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980129/memory/Pokemon/Mewtwo.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980142/memory/Pokemon/Mew.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980154/memory/Pokemon/Articuno.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980164/memory/Pokemon/Zapdos.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980175/memory/Pokemon/Moltres.png',
+      'https://res.cloudinary.com/beumsk/image/upload/v1547980186/memory/Pokemon/Eevee.png'
+    ],
+    disney: [
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982044/memory/disney/mickey.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982088/memory/disney/mowgli.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982610/memory/disney/tarzan.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982620/memory/disney/simba.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982628/memory/disney/aladin.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982636/memory/disney/blanche%20neige.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982644/memory/disney/alice.png',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982653/memory/disney/peter%20pan.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982663/memory/disney/pinocchio.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982738/memory/disney/raiponce.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982044/memory/disney/mickey.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982088/memory/disney/mowgli.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982610/memory/disney/tarzan.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982620/memory/disney/simba.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982628/memory/disney/aladin.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982636/memory/disney/blanche%20neige.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982644/memory/disney/alice.png',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982653/memory/disney/peter%20pan.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982663/memory/disney/pinocchio.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547982738/memory/disney/raiponce.jpg'
+      ],
+      harrypotter: [
+        'https://res.cloudinary.com/beumsk/image/upload/v1547998926/memory/harrypotter/harry.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547998958/memory/harrypotter/ron.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547998992/memory/harrypotter/hermione.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999106/memory/harrypotter/dumbledore.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999032/memory/harrypotter/malfoy.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999143/memory/harrypotter/voldemort.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999401/memory/harrypotter/rogue.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999196/memory/harrypotter/hagrid.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999271/memory/harrypotter/sirius.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999577/memory/harrypotter/neville.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547998926/memory/harrypotter/harry.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547998958/memory/harrypotter/ron.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547998992/memory/harrypotter/hermione.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999106/memory/harrypotter/dumbledore.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999032/memory/harrypotter/malfoy.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999143/memory/harrypotter/voldemort.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999401/memory/harrypotter/rogue.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999196/memory/harrypotter/hagrid.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999271/memory/harrypotter/sirius.jpg',
+        'https://res.cloudinary.com/beumsk/image/upload/v1547999577/memory/harrypotter/neville.jpg'
+      ]
+    };
+
+    var images = [],
+    tempElt1 = "",
+    tempElt2 = "",
+    click = -1,
+    win = 0, //number of cards clicked of same type
+    score = 0,
+    time = 0;
+
+    var preElt = document.querySelector("#pre"),
+    themesElt = document.querySelector("#themes"),
+    boxElts = document.getElementsByClassName("box"),
+    mainElt = document.querySelector(".main"),
+    timeElt = document.querySelector(".time"),
+    scoreElt = document.querySelector(".score"),
+    postElt = document.querySelector("#post"),
+    finalElt = document.querySelector("#final"),
+    againElt = document.querySelector("#again");
+
+
+    // initiate the game with chosen theme
+    themesElt.addEventListener("click", function(e) {
+        if (e.target.classList.contains("themes")) {
+          activateTheme(e.target.id);
+          preElt.classList.add("hidden");
+        }
+    });
+
+
+    function activateTheme(theme) {
+        // insert theme in images array
+      switch (theme) {
+        case "pokemon":
+          for (let i=0; i<20; i++) {images.push(library.pokemon[i]);}
+          break;
+        case "disney":
+          for (let i=0; i<20; i++) {images.push(library.disney[i]);}
+          break;
+        case "harrypotter":
+          for (let i=0; i<20; i++) {images.push(library.harrypotter[i]);}
+          break;
+        // case "tomandjerry":
+        //   for (let i=0; i<20; i++) {images.push(library.tomandjerry[i]);}
+        //   break;
+      }
+          // insert images in memory game
+      for (let i=0; i<20; i++) {
+        var rand = Math.floor(Math.random() * (images.length-1));
+        boxElts[i].innerHTML = "<img src='" + images[rand] + "' alt='image' class='hidden'>";
+        images.splice(rand, 1);
+      }
+    }
+
+    // handle the play
+    mainElt.addEventListener("click", gameLogic);
+
+    function gameLogic(e) {
+    // make sure the box is playable
+      if (e.target.classList.contains("play")) {
+          e.target.firstChild.classList.remove("hidden");
+          // first of two click
+          if (click < 1) {
+            tempElt1 = e.target;
+          // timer
+            if (click === -1) {
+              timer = setInterval(function() {
+                time++;
+                timeElt.innerHTML = time;
+              }, 1000);
+            }
+            click = 1;
+          }
+
+            // second click
+          else if (e.target !== tempElt1) {
+            tempElt2 = e.target;
+
+            // different images
+            if (tempElt1.firstChild.src !== tempElt2.firstChild.src) {
+                mainElt.removeEventListener("click", gameLogic);
+                setTimeout( function() {
+                  tempElt1.firstChild.classList.add("hidden");
+                  tempElt2.firstChild.classList.add("hidden");
+                  mainElt.addEventListener("click", gameLogic);
+                }, 400);
+                if (score > 0){
+                  score -= 1;
+                }
+              scoreElt.innerHTML = score;
+            }
+
+          // same images
+            else {
+              score += 10;
+              win += 2; // number of cards selected same
+              tempElt1.firstChild.classList.add("outlined");
+              tempElt2.firstChild.classList.add("outlined");
+              tempElt1.classList.remove("play");
+              tempElt2.classList.remove("play");
+              scoreElt.innerHTML = score;
+
+                // game won
+              if (win === 20) {
+                clearTimeout(timer);
+                finalElt.innerHTML = "You won " + score + " points <br> in " + time + " seconds";
+                postElt.classList.remove("hidden");
+              }
+            }
+            click = 0;
+          }
+      }
+    }
+
+        againElt.addEventListener("click", resetGame);
+
+        function resetGame() {
+        // reset game
+          tempElt1 = "";
+          tempElt2 = "";
+          click = -1;
+          win = 0;
+          score = 0;
+          time = 0;
+          postElt.classList.add("hidden");
+          preElt.classList.remove("hidden");
+          for (let i=0; i<20; i++) {
+            boxElts[i].classList.add("play");
+            boxElts[i].firstChild.classList.add("hidden");
+          }
+          timeElt.textContent = time;
+          scoreElt.textContent = score;
+        }
